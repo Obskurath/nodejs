3b91b8a270160f4040c2b6304270654b43744841
Eduardo <EduardoVG@proton.me>
2024-11-10 22:23:04 -0600

    Add ESLint configuration files and update package.json dependencies

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+import { log } from 'console';
 import http from 'http'
 
+const routes = {
+    '/':'Formacion Node + Express + MongoDB',
+    '/authors': 'These are the authors',
+    '/books': 'These are the books'
+}
+
 const server = http.createServer((req, res) => {
-    console.log(req.headers)
-    res.writeHead(200, {"content-type" : "text/plain"})
-    res.end('Hello World ') 
+    console.log(req.url);
+    
+    res.writeHead(200, {"content-type" : "text/html"})
+    res.end(routes[req.url]) 
 
 });
 server.listen(3000, () => {
     console.log("Server Ready!")
 })
+
+
