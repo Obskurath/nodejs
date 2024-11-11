58b415972cf190d6f552650a9c71895849a861f1
Eduardo <EduardoVG@proton.me>
2024-11-10 21:22:02 -0600

    Add nodemon and semver modules, update package.json, and include configuration files

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import http from 'http'
 
 const server = http.createServer((req, res) => {
+    console.log(req.headers)
     res.writeHead(200, {"content-type" : "text/plain"})
     res.end('Hello World ') 
 
 });
-server.listen(3000, () => {
+server.listen(3010, () => {
     console.log("Servidor Inciado")
 })
 
+asdasd
